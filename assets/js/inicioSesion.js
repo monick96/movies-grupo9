5e9cd028fe2b39ed8fd4b5b7d51aa6d42355ed79
Juan Bautista Ballini <jbballini@hotmail.com.ar>
2024-05-19 17:32:23 -0300

    arreglos 3

--- assets/js/inicioSesion.js
+++ assets/js/inicioSesion.js
@@ -3,9 +3,13 @@ const password = document.getElementById('password');
 const boton = document.getElementById('boton-enviar');
 
 boton.addEventListener('click', (evento) => {
-
+    
+    evento.preventDefault();
+    
     if (username.value == "admin" && password.value == "admin")
         alert("Usted a Iniciado Sesión correctamente");
     else
         document.getElementById('datos-incorrectos').classList.replace("text-datosIncorrectos", "text-datosIncorrectos-mostrar");
+    
+        
 })
